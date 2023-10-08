3095b3faee798a8620fcd0bf82d7e609595f49de
Vinícius Yoda <90533635+ViniciusYoda@users.noreply.github.com>
2023-10-08 11:00:06 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/InventarioWeb/InventarioWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.InventarioWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/InventarioWeb/InventarioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.InventarioWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
